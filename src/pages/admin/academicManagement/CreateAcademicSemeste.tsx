6c48b91058f7cfbc87a4f952c48452259d611ab1
abdullah-mamun369 <maamamun369@gmail.com>
2024-12-26 14:53:33 +0600

    Select Component is done

--- src/pages/admin/academicManagement/CreateAcademicSemeste.tsx
+++ src/pages/admin/academicManagement/CreateAcademicSemeste.tsx
@@ -2,6 +2,7 @@ import { FieldValues, SubmitHandler } from "react-hook-form";
 import PHForm from "../../../components/form/PHForm";
 import PHInput from "../../../components/form/PHInput";
 import { Button } from "antd";
+import PHSelect from "../../../components/form/PHSelect";
 
 const CreateAcademicSemeste = () => {
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
@@ -10,8 +11,13 @@ const CreateAcademicSemeste = () => {
 
   return (
     <PHForm onSubmit={onSubmit}>
-      <PHInput type="text" name="year" label="Year" />;
-      <Button htmlType="submit">Submit</Button>
+      <PHInput type="text" name="year" label="Year" />
+      <PHSelect
+        label="Name"
+        name="name"
+        options={[{ value: "1", label: "1st Semester", disabled: false }]}
+      />
+      ;<Button htmlType="submit">Submit</Button>
     </PHForm>
   );
 };
