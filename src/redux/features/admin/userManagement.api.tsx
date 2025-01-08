c1f1e8f743cd0c051508e4940b84975391d4d252
abdullah-mamun369 <maamamun369@gmail.com>
2025-01-08 16:08:14 +0600

    Basic table and basic redux api decleared for get students

--- src/redux/features/admin/userManagement.api.tsx
+++ src/redux/features/admin/userManagement.api.tsx
@@ -1,31 +1,32 @@
+import { TQueryParam } from "../../../types";
 import { baseApi } from "../../api/baseApi";
 
 const userManagementApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
-    // //Get all academic semesters----------------------------------------------------------------
-    // getAllSemesters: builder.query({
-    //   query: (args) => {
-    //     const params = new URLSearchParams();
+    //Get all academic semesters----------------------------------------------------------------
+    getAllStudents: builder.query({
+      query: (args) => {
+        const params = new URLSearchParams();
 
-    //     if (args) {
-    //       args.forEach((item: TQueryParam) => {
-    //         params.append(item.name, item.value as string);
-    //       });
-    //     }
+        if (args) {
+          args.forEach((item: TQueryParam) => {
+            params.append(item.name, item.value as string);
+          });
+        }
 
-    //     return {
-    //       url: "/academic-semesters",
-    //       method: "GET",
-    //       params: params,
-    //     };
-    //   },
-    //   transformResponse: (response: TResponseRedux<TAcademicSemester[]>) => {
-    //     return {
-    //       data: response.data,
-    //       meta: response.meta,
-    //     };
-    //   },
-    // }),
+        return {
+          url: "/academic-semesters",
+          method: "GET",
+          params: params,
+        };
+      },
+      transformResponse: (response: TResponseRedux<TAcademicSemester[]>) => {
+        return {
+          data: response.data,
+          meta: response.meta,
+        };
+      },
+    }),
 
     //Create academic semester----------------------------------------------------------------
     addStudent: builder.mutation({
