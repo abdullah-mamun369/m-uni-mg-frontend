e55d58b7745bbc9cdce065c4b318f88f0279b6ff
abdullah-mamun369 <maamamun369@gmail.com>
2025-01-14 19:22:29 +0600

    Semester options are shown to the semester registration

--- src/pages/admin/courseManagement/SemesterRegistration.tsx
+++ src/pages/admin/courseManagement/SemesterRegistration.tsx
@@ -1,5 +1,84 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { FieldValues, SubmitHandler } from "react-hook-form";
+import PHForm from "../../../components/form/PHForm";
+import { Button, Col, Flex } from "antd";
+import PHSelect from "../../../components/form/PHSelect";
+import { semesterOptions } from "../../../constants/semester";
+import { monthOptions } from "../../../constants/global";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { academicSemesterSchema } from "../../../schemas/academicManagement.schema";
+import { toast } from "sonner";
+import { TResponse } from "../../../types/global";
+import { TAcademicSemester } from "../../../types/academicManagement.type";
+import { useGetAllSemestersQuery } from "../../../redux/features/admin/academicManagement.api";
+
 const SemesterRegistration = () => {
-  return <div>This is SemesterRegistration component</div>;
+  const { data: academicSemester } = useGetAllSemestersQuery([
+    { name: "sort", value: "year" },
+  ]);
+
+  const academicSemesterOptions = academicSemester?.data?.map((item) => ({
+    value: item._id,
+    label: `${item.name} ${item.year}`,
+  }));
+
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    const toastId = toast.loading("Creating semester");
+
+    //1st way to get the name
+    const name = semesterOptions.find(
+      (item) => item.value === data.name
+    )?.label;
+
+    //2nd way to get the name
+    // const name = semesterOptions[Number(data?.name) - 1].label;
+
+    const semesterData = {
+      name,
+      code: data.name,
+      year: data.year,
+      startMonth: data.startMonth,
+      endMonth: data.endMonth,
+    };
+
+    // try {
+    //   const res = (await addAcademicSemester(
+    //     semesterData
+    //   )) as TResponse<TAcademicSemester>;
+    //   if (res.error) {
+    //     toast.error(res.error.data.message, { id: toastId });
+    //   } else {
+    //     toast.success("Semester created", { id: toastId });
+    //   }
+    // } catch (err) {
+    //   toast.error("Something went wrong", { id: toastId });
+    // }
+  };
+
+  return (
+    <Flex justify="center" align="center">
+      <Col span={6}>
+        <PHForm
+          onSubmit={onSubmit}
+          resolver={zodResolver(academicSemesterSchema)}
+        >
+          <PHSelect
+            label="Name"
+            name="name"
+            options={academicSemesterOptions}
+          />
+          <PHSelect
+            label="Start Month"
+            name="startMonth"
+            options={monthOptions}
+          />
+          <PHSelect label="End Month" name="endMonth" options={monthOptions} />
+
+          <Button htmlType="submit">Submit</Button>
+        </PHForm>
+      </Col>
+    </Flex>
+  );
 };
 
 export default SemesterRegistration;
