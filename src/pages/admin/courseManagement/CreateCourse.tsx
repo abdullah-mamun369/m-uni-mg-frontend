bbc56ad654437faa350ccd7751bc49db16d2d6d2
abdullah-mamun369 <maamamun369@gmail.com>
2025-01-15 14:00:11 +0600

    Basic Dtructure for course create is done without multiple select

--- src/pages/admin/courseManagement/CreateCourse.tsx
+++ src/pages/admin/courseManagement/CreateCourse.tsx
@@ -1,5 +1,62 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { FieldValues, SubmitHandler } from "react-hook-form";
+import PHForm from "../../../components/form/PHForm";
+import { Button, Col, Flex } from "antd";
+import PHSelect from "../../../components/form/PHSelect";
+import { semesterStatusOptions } from "../../../constants/semester";
+import { toast } from "sonner";
+import { TResponse } from "../../../types/global";
+import { useGetAllSemestersQuery } from "../../../redux/features/admin/academicManagement.api";
+import PHDatePicker from "../../../components/form/PHDatePicker";
+import PHInput from "../../../components/form/PHInput";
+import { useAddRegisteredSemesterMutation } from "../../../redux/features/admin/courseManagement.api";
+
 const CreateCourse = () => {
-  return <div>This is CreateCourse component</div>;
+  const [addSemester] = useAddRegisteredSemesterMutation();
+
+  const { data: academicSemester } = useGetAllSemestersQuery([
+    { name: "sort", value: "year" },
+  ]);
+
+  const academicSemesterOptions = academicSemester?.data?.map((item) => ({
+    value: item._id,
+    label: `${item.name} ${item.year}`,
+  }));
+
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    const toastId = toast.loading("Creating...");
+
+    const semesterData = {
+      ...data,
+      minCredit: Number(data.minCredit),
+      maxCredit: Number(data.maxCredit),
+    };
+
+    try {
+      const res = (await addSemester(semesterData)) as TResponse<any>;
+      if (res.error) {
+        toast.error(res.error.data.message, { id: toastId });
+      } else {
+        toast.success("Semester created", { id: toastId });
+      }
+    } catch (err) {
+      toast.error("Something went wrong", { id: toastId });
+    }
+  };
+
+  return (
+    <Flex justify="center" align="center">
+      <Col span={6}>
+        <PHForm onSubmit={onSubmit}>
+          <PHInput type="text" name="title" label="Title" />
+          <PHInput type="text" name="prefix" label="Prefix" />
+          <PHInput type="text" name="code" label="Code" />
+          <PHInput type="text" name="credits" label="Credits" />
+          <Button htmlType="submit">Submit</Button>
+        </PHForm>
+      </Col>
+    </Flex>
+  );
 };
 
 export default CreateCourse;
